ba968926f2e0b95ecfd11d31ed3ee3a5d976c8fe
jens-o <Jens@iMac.local>
2016-12-31 11:37:09 +0100

    Initial Commit

---
+++ RCHub.ino
@@ -0,0 +1,188 @@
+/*
+  RC Hub
+  
+  Written in 2016 by Jens Olsson.
+*/
+
+// Import required libraries
+#include <WiFiEsp.h>
+#include <SoftwareSerial.h>
+#include "HomeEasy.h"
+
+// Create Serial for the WiFi card
+SoftwareSerial Serial1(6, 7);
+
+int status = WL_IDLE_STATUS;
+
+// Create an HomeEasy instance
+HomeEasy homeEasy;
+
+// WiFi parameters
+const char* ssid = "K16";
+const char* pass = "jingijamborii";
+
+// Default NEXA remote ID
+unsigned long remoteId = 22611714;
+
+// The port to listen for incoming TCP connections
+#define LISTEN_PORT           80
+
+// Create an instance of the server
+WiFiEspServer server(LISTEN_PORT);
+
+void setup(void)
+{
+  // Start Serial
+  Serial.begin(115200);
+
+  Serial1.begin(9600);
+
+  // Initate HomeEasy
+  homeEasy = HomeEasy();
+  
+  homeEasy.registerSimpleProtocolHandler(printSimpleResult);
+  homeEasy.registerAdvancedProtocolHandler(printAdvancedResult);
+  
+  homeEasy.init();
+
+  // Initiate WiFi
+  WiFi.init(&Serial1);
+
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+
+    while(true);
+  }
+
+  while (status != WL_CONNECTED) {
+    Serial.print("Attemting to connect to SSID: ");
+    Serial.println(ssid);
+
+    status = WiFi.begin(ssid, pass);
+  }
+
+  Serial.println("You're connected to the network");
+  printWiFiStatus();
+
+  // Start the server
+  server.begin();
+}
+
+void loop() {
+
+  // Handle REST calls
+  WiFiEspClient client = server.available();
+
+  if (client) {
+    if (client.connected()) {
+      if (client.available()) {
+        process(client);
+      }
+  
+      delay(10);
+  
+      client.stop();
+    }
+  }
+}
+
+void process(WiFiEspClient client) {
+
+  // Jump over HTTP action
+  client.readStringUntil('/');
+
+  // Get the command
+  String command = client.readStringUntil('/');
+
+  if (command == "SwitchOn") {
+    switchOnOff(true, client);
+  }
+  else if (command == "SwitchOff") {
+    switchOnOff(false, client);
+  }
+  else {
+    Serial.print("Received unknown command: ");
+    Serial.println(command);
+
+    client.print(
+            "HTTP/1.1 400 Bad Request\r\n"
+            "Content-Type: text/html\r\n"
+            "Connection: close\r\n"
+            "\r\n");
+    return;
+  }
+
+  client.print(
+            "HTTP/1.1 200 OK\r\n"
+            "Content-Type: text/html\r\n"
+            "Connection: close\r\n"
+            "\r\n");
+}
+
+// Custom function accessible by the API
+int switchOnOff(bool on, WiFiEspClient client) {
+
+  // Get first param
+  unsigned int device = client.parseInt();
+  
+  bool group = false;
+
+  Serial.print("switchOnOff() called: ");
+  Serial.print(device);
+  Serial.print(":");
+  Serial.println(on);
+  
+  homeEasy.sendAdvancedProtocolMessage(remoteId, device, on, group);
+  //homeEasy.sendAdvancedProtocolMessage(remoteId, 1, on, group);
+}
+
+/**
+ * Print the details of the advanced protocol message.
+ */
+void printAdvancedResult(unsigned long sender, unsigned int recipient, bool on, bool group)
+{
+  Serial.println("advanced protocol message");
+  
+  Serial.print("sender ");
+  Serial.println(sender);
+  
+  Serial.print("recipient ");
+  Serial.println(recipient);
+  
+  Serial.print("on ");
+  Serial.println(on);
+  
+  Serial.print("group ");
+  Serial.println(group);
+  
+  Serial.println();
+}
+
+
+/**
+ * Print the details of the simple protocol message.
+ */
+void printSimpleResult(unsigned int sender, unsigned int recipient, bool on)
+{
+  Serial.println("simple protocol message");
+  
+  Serial.print("sender ");
+  Serial.println(sender);
+  
+  Serial.print("recipient ");
+  Serial.println(recipient);
+  
+  Serial.print("on ");
+  Serial.println(on);
+  
+  Serial.println();
+}
+
+void printWiFiStatus() {
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  IPAddress ip = WiFi.localIP();
+  Serial.print("AP Address: ");
+  Serial.println(ip);
+}
